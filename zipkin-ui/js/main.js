3ca1f743155302355ffeb95d2995a29d8386dbee
xianliu <xianliu007@126.com>
2018-05-23 16:22:58 +0800

    自定义页面

--- zipkin-ui/js/main.js
+++ zipkin-ui/js/main.js
@@ -10,6 +10,7 @@ import initializeDefault from './page/default';
 import initializeTrace from './page/trace';
 import initializeTraceViewer from './page/traceViewer';
 import initializeDependency from './page/dependency';
+import initializeTraceLogs from './page/tracesLogs';
 import CommonUI from './page/common';
 import loadConfig from './config';
 import {errToStr} from './component_ui/error';
@@ -24,6 +25,7 @@ loadConfig().then(config => {
   crossroads.addRoute(`${contextRoot}traces/{id}`, traceId => initializeTrace(traceId, config));
   crossroads.addRoute(`${contextRoot}traceViewer`, () => initializeTraceViewer(config));
   crossroads.addRoute(`${contextRoot}dependency`, () => initializeDependency(config));
+  crossroads.addRoute(`${contextRoot}tracesLogs`, () => initializeTraceLogs(config))
   crossroads.parse(window.location.pathname);
 }, e => {
   // TODO: better error message, but this is better than a blank screen...
---
+++ zipkin-ui/js/page/tracesLogs.js
@@ -0,0 +1,17 @@
+import {component} from 'flightjs';
+import {tracesLogsTemplate} from '../templates';
+import {contextRoot} from "../publicPath";
+const TraceLogsPageComponent = component(function TraceLogsPage(){
+  this.after('initialize', function() {
+    this.on(document, 'traceLogsPageModelView', function(ev, data) {
+      this.$node.html(tracesLogsTemplate({
+        contextRoot,
+        ...data.modelview
+      }));
+    });
+  });
+});
+
+export default function initializeTrace(config) {
+  TraceViewerPageComponent.attachTo('.content', {config});
+}
