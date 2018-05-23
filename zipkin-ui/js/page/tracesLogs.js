cd0b50922b4b805c260f0ce484fcfce95a4d75e5
xianliu <xianliu007@126.com>
2018-05-23 17:52:57 +0800

    自定义

--- zipkin-ui/js/page/tracesLogs.js
+++ zipkin-ui/js/page/tracesLogs.js
@@ -1,9 +1,53 @@
 import {component} from 'flightjs';
+import ServiceFilterSearchUI from "../component_ui/serviceFilterSearch";
+import ZoomOut from "../component_ui/zoomOutSpans";
+import {traceTemplate} from "../templates";
+import $ from "jquery";
+import SpanPanelUI from "../component_ui/spanPanel";
+import FilterAllServicesUI from "../component_ui/filterAllServices";
+import JsonPanelUI from "../component_ui/jsonPanel";
+import TraceUI from "../component_ui/trace";
+import FullPageSpinnerUI from "../component_ui/fullPageSpinner";
+import FilterLabelUI from "../component_ui/filterLabel";
+import {contextRoot} from "../publicPath";
+import TraceData from "../component_data/trace";
 
 const TraceLogsPageComponent = component(function TraceLogsPage() {
-  /* eslint prefer-arrow-callback: ["error", { "allowNamedFunctions": true }] */
   this.after('initialize', function() {
-    console.log('xxx');
+    window.document.title = 'Zipkin - Traces';
+
+    TraceData.attachTo(document, {
+      traceId: this.attr.traceId,
+      logsUrl: this.attr.config('logsUrl')
+    });
+    this.on(document, 'tracePageModelView', function(ev, data) {
+      this.$node.html(traceTemplate({
+        contextRoot,
+        ...data.modelview
+      }));
+
+      FilterAllServicesUI.attachTo('#filterAllServices', {
+        totalServices: $('.trace-details.services span').length
+      });
+      FullPageSpinnerUI.attachTo('#fullPageSpinner');
+      JsonPanelUI.attachTo('#jsonPanel');
+      ServiceFilterSearchUI.attachTo('#serviceFilterSearch');
+      SpanPanelUI.attachTo('#spanPanel');
+      TraceUI.attachTo('#trace-container');
+      FilterLabelUI.attachTo('.service-filter-label');
+      ZoomOut.attachTo('#zoomOutSpans');
+
+      this.$node.find('#traceJsonLink').click(e => {
+        e.preventDefault();
+        this.trigger('uiRequestJsonPanel', {
+          title: `Trace ${this.attr.traceId}`,
+          obj: data.trace,
+          link: `${contextRoot}api/v1/trace/${this.attr.traceId}`
+        });
+      });
+
+      $('.annotation:not(.core)').tooltip({placement: 'left'});
+    });
   });
 });
 
