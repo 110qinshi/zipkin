e0610cf90073738d189ae4c0050beece03f6f9e2
xianliu <xianliu007@126.com>
2018-05-24 11:53:57 +0800

    自定义

--- zipkin-ui/js/page/tracesLogs.js
+++ zipkin-ui/js/page/tracesLogs.js
@@ -1,14 +1,6 @@
 import {component} from 'flightjs';
-import ServiceFilterSearchUI from '../component_ui/serviceFilterSearch';
-import ZoomOut from '../component_ui/zoomOutSpans';
 import {tracesLogsTemplate} from '../templates';
 import $ from 'jquery';
-import SpanPanelUI from '../component_ui/spanPanel';
-import FilterAllServicesUI from '../component_ui/filterAllServices';
-import JsonPanelUI from '../component_ui/jsonPanel';
-import TraceUI from '../component_ui/trace';
-import FullPageSpinnerUI from '../component_ui/fullPageSpinner';
-import FilterLabelUI from '../component_ui/filterLabel';
 import {contextRoot} from '../publicPath';
 import TraceLogData from '../component_data/tracesLogs';
 
@@ -22,17 +14,6 @@ const TraceLogsPageComponent = component(function TraceLogsPage() {
         contextRoot,
         ...data.modelview
       }));
-
-      FilterAllServicesUI.attachTo('#filterAllServices', {
-        totalServices: $('.trace-details.services span').length
-      });
-      FullPageSpinnerUI.attachTo('#fullPageSpinner');
-      JsonPanelUI.attachTo('#jsonPanel');
-      ServiceFilterSearchUI.attachTo('#serviceFilterSearch');
-      SpanPanelUI.attachTo('#spanPanel');
-      TraceUI.attachTo('#trace-container');
-      FilterLabelUI.attachTo('.service-filter-label');
-      ZoomOut.attachTo('#zoomOutSpans');
 
       this.$node.find('#traceJsonLink').click(e => {
         e.preventDefault();
