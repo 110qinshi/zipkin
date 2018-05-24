44d0973fc27a4979ba1c4327007ecedeab39d2bb
xianliu <xianliu007@126.com>
2018-05-24 11:29:00 +0800

    自定义

--- zipkin-ui/js/component_data/tracesLogs.js
+++ zipkin-ui/js/component_data/tracesLogs.js
@@ -1,12 +1,5 @@
 import {component} from 'flightjs';
 
-export function toContextualLogsUrl(logsUrl, traceId) {
-  if (logsUrl) {
-    return logsUrl.replace('{traceId}', traceId);
-  }
-  return logsUrl;
-}
-
 export default component(function TraceData() {
   this.after('initialize', function() {
     this.trigger(document, 'traceLogPageModelView', {name: '55'});
